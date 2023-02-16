1cc4e4ca6e09a51ccff7c69f60f590c12d85bf93
KHT@air <tan.k.h@juno.com>
2023-02-16 15:41:47 -0800

    trace set to retain on failure

--- dw1/playwright/config/playwright.config.ts
+++ dw1/playwright/config/playwright.config.ts
@@ -39,11 +39,11 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
+    trace: 'retain-on-failure'
   },
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: 'test-results/'
+  outputDir: '../test-results/'
 
   /* Run your local dev server before starting the tests */
   // webServer: {
