2ea154d46d611b2264dcc574559610e56a7e431d
Kwee Tan <kwee@eaglecapsoftware.com>
2025-09-05 14:36:21 -0700

    icB pylint fixes

--- iching/icB/lib/core.py
+++ iching/icB/lib/core.py
@@ -1,9 +1,11 @@
-# lib/core.py
+""" lib/core.py """
 from typing import TypeVar
 HaveAddOperator = TypeVar('HaveAddOperator', int, float, str, bytes)
 T1 = HaveAddOperator
 def greet(name: str) -> str:
+    """ Returns a greeting message """
     return f"Hello, {name}"
 
 def add(a: T1, b: T1) -> T1:
+    """ Returns the + of two values """
     return a + b
