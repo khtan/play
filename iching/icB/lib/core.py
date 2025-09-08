62b932940e4bcde04b34c32b2af6a4da5b6373fb
Kwee Tan <kwee@eaglecapsoftware.com>
2025-09-08 11:09:38 -0700

    view_file to handle all exceptions

--- iching/icB/lib/core.py
+++ iching/icB/lib/core.py
@@ -1,4 +1,5 @@
 """ lib/core.py """
+import os
 from typing import TypeVar
 HaveAddOperator = TypeVar('HaveAddOperator', int, float, str, bytes)
 T1 = HaveAddOperator
@@ -9,3 +10,14 @@ def greet(name: str) -> str:
 def add(a: T1, b: T1) -> T1:
     """ Returns the + of two values """
     return a + b
+
+def view_file(file_path: str) -> None:
+    """ Brings up Windows default app on the file_path """
+    try:
+        os.startfile(file_path)
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+    except (IOError, OSError) as e:
+        print(f"IOError or OSError on {file_path}: {e}")
+    except Exception as e: # pylint: disable=W0718
+        print(f"An unexpected error occurred: {e}")
